2b5acd856e7ac9d8b436a5682b0af8223e4395fd
atmq1020 <atmq1020@gmail.com>
2025-12-19 21:48:39 +0100

    fix: parsing parametrized tests (#21)

--- lua/neotest-jdtls/utils/test_context.lua
+++ lua/neotest-jdtls/utils/test_context.lua
@@ -1,4 +1,5 @@
 local class = require('neotest-jdtls.utils.class')
+local log = require('neotest-jdtls.utils.log')
 
 ---@class TestContext
 ---@field lookup table<string, table<string, table<string, JavaTestItem>>>
@@ -12,7 +13,9 @@ end
 
 ---@param test_item JavaTestItem
 function TestContext:append_test_item(key, test_item)
-	self.lookup[test_item.id] = { key = key, value = test_item }
+	local id = test_item.id:gsub('[<>%s]', '')
+	log.debug('Cleaned test item id: ', id, ' -> ', test_item.id)
+	self.lookup[id] = { key = key, value = test_item }
 end
 
 return TestContext
